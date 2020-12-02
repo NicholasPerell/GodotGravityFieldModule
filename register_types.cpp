ba2e6331be45bd5841862f69e80238ba0fd9b330
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2020-12-02 09:13:00 -0500

    Adding module base files - Nick

---
+++ register_types.cpp
@@ -0,0 +1,10 @@
+#include "register_types.h"
+#include "core/class_db.h"
+#include "2DMeshSubdivisionNode.h"
+
+void register_Godot-Gravity-Field-Module_types(){
+	//ClassDB::register_class<SubdivisionNode>();
+}
+
+void unregister_Godot-Gravity-Field-Module_types() {
+}
---
+++ register_types.h
@@ -0,0 +1,9 @@
+void register_Godot-Gravity-Field-Module_types();
+void unregister_Godot-Gravity-Field-Module_types();
+
+/*
+	void register_MODULNAME_types();
+	void unregister_MODULNAME_types();
+
+	MODULNAME ->> insert the name of your module
+*/
