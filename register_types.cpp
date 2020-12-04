1399be74ff6271fa55e7fb88737319735de03eec
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2020-12-04 10:55:42 -0500

    Defined EllipseGravityField2D

--- register_types.cpp
+++ register_types.cpp
@@ -2,12 +2,11 @@
 #include "core/class_db.h"
 #include "GravityField2D.h"
 #include "SquareGravityField2D.h"
-//#include "2DMeshSubdivisionNode.h"
+#include "EllipseGravityField2D.h"
 
 void register_GodotGravityFieldModule_types(){
-	//ClassDB::register_class<SubdivisionNode>();
-	//ClassDB::register_class<GravityField2D>();
 	ClassDB::register_class<SquareGravityField2D>();
+	ClassDB::register_class<EllipseGravityField2D>();
 }
 
 void unregister_GodotGravityFieldModule_types() {
