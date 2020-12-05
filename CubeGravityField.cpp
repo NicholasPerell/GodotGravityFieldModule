0cdc57982d39a9ea2e27552725f90e97116de0c2
Amelia <ameliappayne@gmail.com>
2020-12-05 10:27:28 -0500

    Worked on cube gravity field

--- CubeGravityField.cpp
+++ CubeGravityField.cpp
@@ -13,17 +13,40 @@ Vector3 CubeGravityField::calcForce(RigidBody *body) {
 }
 
 //TODO: Add getters and setters for prameters
-void CubeGravityField::setA(float aPram) {
-	a = aPram;
+void CubeGravityField::setLength(float l) {
+	length = l;
 }
-float CubeGravityField::getA() {
-	return a;
+float CubeGravityField::getLength() {
+	return length;
+}
+
+void CubeGravityField::setWidth(float w) {
+	width = w;
+}
+float CubeGravityField::getWidth() {
+	return width;
+}
+
+void CubeGravityField::setHeight(float h) {
+	height = h;
+}
+float CubeGravityField::getHeight() {
+	return height;
 }
 
 //Bind getters and setters and add paramters as property
 void CubeGravityField::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("setA", "h"), &CubeGravityField::setA);
-	ClassDB::bind_method(D_METHOD("getA"), &CubeGravityField::getA);
+	ClassDB::bind_method(D_METHOD("setLength", "l"), &CubeGravityField::setLength);
+	ClassDB::bind_method(D_METHOD("getLength"), &CubeGravityField::getLength);
 
-	ADD_PROPERTY(PropertyInfo(Variant::REAL, "a", PROPERTY_HINT_RANGE, "-1024,1024,0.001"), "setA", "getA");
+	ClassDB::bind_method(D_METHOD("setWidth", "w"), &CubeGravityField::setWidth);
+	ClassDB::bind_method(D_METHOD("getWidth"), &CubeGravityField::getWidth);
+
+	ClassDB::bind_method(D_METHOD("setHeight", "h"), &CubeGravityField::setHeight);
+	ClassDB::bind_method(D_METHOD("getHeight"), &CubeGravityField::getHeight);
+
+
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "Length", PROPERTY_HINT_RANGE, "-1024,1024,0.001"), "setLength", "getLength");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "Width", PROPERTY_HINT_RANGE, "-1024,1024,0.001"), "setWidth", "getWidth");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "Height", PROPERTY_HINT_RANGE, "-1024,1024,0.001"), "setHeight", "getHeight");
 }
--- CubeGravityField.h
+++ CubeGravityField.h
@@ -6,13 +6,22 @@ class RigidBody;
 class CubeGravityField : public GravityField {
 	GDCLASS(CubeGravityField, GravityField);
 
-	float a;
+	float length;
+	float width;
+	float height;
+
 
 protected:
 	static void _bind_methods();
 
 public:
 	virtual Vector3 calcForce(RigidBody *body);
-	void setA(float aPram);
-	float getA();
+	void setLength(float l);
+	float getLength();
+
+	void setWidth(float w);
+	float getWidth();
+
+	void setHeight(float h);
+	float getHeight();
 };
