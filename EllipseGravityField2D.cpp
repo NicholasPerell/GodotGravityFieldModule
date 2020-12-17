8be91e6bb16b913c88f0529f92ad1c7f3e0f88b5
Brandon L'Abbe <brandon.labbe@mymail.champlain.edu>
2020-12-17 14:19:30 -0500

    updated elipse

--- EllipseGravityField2D.cpp
+++ EllipseGravityField2D.cpp
@@ -1,5 +1,6 @@
 #include "EllipseGravityField2D.h"
 #include <scene\2d\physics_body_2d.h>
+#include <math.h>
 
 void EllipseGravityField2D::_bind_methods()
 {
@@ -14,10 +15,17 @@ Vector2 EllipseGravityField2D::calcForce(RigidBody2D* body)
 	Vector2 diff = body->get_global_position() - get_global_position();
 	//diff = Vector2(diff.x / axes.x, diff.y / axes.y);
 
-	diff = Vector2(diff.x, diff.y / (axes.y * axes.y));
-	diff.normalize();
-	diff *= -gravityScale * body->get_gravity_scale();
-	return diff;
+	float a, b;
+
+	(axes.x > axes.y) ? a = axes.x : a = axes.y; //A is the major radius, B is the minor one
+	(axes.x < axes.y) ? b = axes.x : b = axes.y;
+
+	float phi = Math::atan2(diff.y * a * a, diff.x * b * b);
+
+	Vector2 dir = -Vector2(cos(phi), sin(phi));
+
+	dir *= -gravityScale * body->get_gravity_scale();
+	return dir;
 }
 
 void EllipseGravityField2D::setAxes(Vector2 a)
